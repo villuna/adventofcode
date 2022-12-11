732ca1f07290af6574457af4a097c798dad8855f
Luna Borella <ulfrthewolf@gmail.com>
2022-12-11 18:00:20 +1000

    More day eleven stuff

--- 2022/rust/src/dayeleven.rs
+++ 2022/rust/src/dayeleven.rs
@@ -1,126 +1,106 @@
+use itertools::Itertools;
+use regex::Regex;
 use std::collections::VecDeque;
 
-const ROUNDS_P1: u32 = 20;
-const ROUNDS_P2: u32 = 10000;
-
-#[derive(Clone)]
-struct Monkey {
-    items: VecDeque<u64>,
-    operation: fn(u64) -> u64,
-    divisor: u64,
-    next_monkeys: (usize, usize),
+// The reason i moved to a vec of structs isn't for any high minded
+// data oriented reason or whatever
+// it's because Box<dyn Fn(u64) -> u64> doesn't implement Clone
+struct Monkeys {
+    items: Vec<VecDeque<u64>>,
+    operations: Vec<Box<dyn Fn(u64) -> u64>>,
+    divisors: Vec<u64>,
+    next_monkeys: Vec<(usize, usize)>,
 }
 
-pub fn day_eleven(_input: String) {
-    // Hardcoded monkeys to start with for speed
-    // I will parse them properly later
-    let monkeys = vec![
-        Monkey {
-            items: VecDeque::from([85, 79, 63, 72]),
-            operation: |old| old * 17,
-            divisor: 2,
-            next_monkeys: (2, 6),
-        },
-        Monkey {
-            items: VecDeque::from([53, 94, 65, 81, 93, 73, 57, 92]),
-            operation: |old| old * old,
-            divisor: 7,
-            next_monkeys: (0, 2),
-        },
-        Monkey {
-            items: VecDeque::from([62, 63]),
-            operation: |old| old + 7,
-            divisor: 13,
-            next_monkeys: (7, 6),
-        },
-        Monkey {
-            items: VecDeque::from([57, 92, 56]),
-            operation: |old| old + 4,
-            divisor: 5,
-            next_monkeys: (4, 5),
-        },
-        Monkey {
-            items: VecDeque::from([67]),
-            operation: |old| old + 5,
-            divisor: 3,
-            next_monkeys: (1, 5),
-        },
-        Monkey {
-            items: VecDeque::from([85, 56, 66, 72, 57, 99]),
-            operation: |old| old + 6,
-            divisor: 19,
-            next_monkeys: (1, 0),
-        },
-        Monkey {
-            items: VecDeque::from([86, 65, 98, 97, 69]),
-            operation: |old| old * 13,
-            divisor: 11,
-            next_monkeys: (3, 7),
-        },
-        Monkey {
-            items: VecDeque::from([87, 68, 92, 66, 91, 50, 68]),
-            operation: |old| old + 2,
-            divisor: 17,
-            next_monkeys: (4, 3),
-        },
-    ];
+fn parse_monkeys(input: &str) -> Monkeys {
+    let re: Regex = Regex::new(
+        r"Monkey \d+:\n\s*Starting items: ((?:\d+)(?:, \d+)*)
+\s*Operation: new = old (\*|\+) (\d+|old)
+\s*Test: divisible by (\d+)
+\s*If true: throw to monkey (\d+)
+\s*If false: throw to monkey (\d+)",
+    )
+    .unwrap();
 
-    part_one(monkeys.clone());
-    part_two(monkeys);
-}
+    let mut monkeys = Monkeys {
+        items: vec![],
+        operations: vec![],
+        divisors: vec![],
+        next_monkeys: vec![],
+    };
 
-fn part_one(mut monkeys: Vec<Monkey>) {
-    // Part 1
-    let num_monkeys = monkeys.len();
-    let mut inspections = vec![0; num_monkeys];
+    for captures in re.captures_iter(input) {
+        let (items, operator, operand, divisor, true_monkey, false_monkey) = captures
+            .iter()
+            .skip(1)
+            .map(|m| m.unwrap().as_str().to_string())
+            .collect_tuple()
+            .unwrap();
 
-    for _ in 0..ROUNDS_P1 {
-        for i in 0..num_monkeys {
-            while let Some(item) = monkeys[i].items.pop_front() {
-                inspections[i] += 1;
+        monkeys.items.push(VecDeque::from_iter(
+            items.split(", ").map(|num| num.parse::<_>().unwrap()),
+        ));
+        monkeys.operations.push(Box::new(move |old| {
+            let operand = if operand == "old" {
+                old
+            } else {
+                operand.parse().unwrap()
+            };
 
-                let new = (monkeys[i].operation)(item);
-                let new = ((new as f64) / 3.0).floor() as u64;
-
-                let next_monkey = if new % monkeys[i].divisor == 0 {
-                    monkeys[i].next_monkeys.0
-                } else {
-                    monkeys[i].next_monkeys.1
-                };
-
-                monkeys[next_monkey].items.push_back(new);
+            if operator == "*" {
+                old * operand
+            } else {
+                old + operand
             }
-        }
+        }));
+
+        monkeys.divisors.push(divisor.parse().unwrap());
+        monkeys
+            .next_monkeys
+            .push((true_monkey.parse().unwrap(), false_monkey.parse().unwrap()));
     }
 
-    inspections.sort();
-    println!(
-        "{}",
-        inspections[num_monkeys - 1] * inspections[num_monkeys - 2]
-    );
+    monkeys
 }
 
-fn part_two(mut monkeys: Vec<Monkey>) {
-    let num_monkeys = monkeys.len();
-    let modulus: u64 = monkeys.iter().map(|monkey| monkey.divisor).product();
+pub fn day_eleven(input: String) {
+    let mut monkeys = parse_monkeys(&input);
+    let items = monkeys.items.clone();
+
+    // Part one
+    simulate_monkeys(&mut monkeys, 20, true);
+
+    // reset the items
+    monkeys.items = items;
+    simulate_monkeys(&mut monkeys, 10000, false);
+}
+
+fn simulate_monkeys(monkeys: &mut Monkeys, rounds: u32, dividing: bool) {
+    let num_monkeys = monkeys.items.len();
+    let modulus: u64 = monkeys.divisors.iter().product();
 
     let mut inspections = vec![0u64; num_monkeys];
 
-    for _ in 0..ROUNDS_P2 {
+    for _ in 0..rounds {
         for i in 0..num_monkeys {
-            while let Some(item) = monkeys[i].items.pop_front() {
+            while let Some(item) = monkeys.items[i].pop_front() {
                 inspections[i] += 1;
 
-                let new = (monkeys[i].operation)(item);
-                let new = new % modulus;
+                let mut new = (monkeys.operations[i])(item);
 
-                let next_monkey = if new % monkeys[i].divisor == 0 {
-                    monkeys[i].next_monkeys.0
+                if dividing {
+                    new = ((new as f64) / 3.0).floor() as u64;
+                }
+
+                new = new % modulus;
+
+                let next_monkey = if new % monkeys.divisors[i] == 0 {
+                    monkeys.next_monkeys[i].0
                 } else {
-                    monkeys[i].next_monkeys.1
+                    monkeys.next_monkeys[i].1
                 };
 
-                monkeys[next_monkey].items.push_back(new);
+                monkeys.items[next_monkey].push_back(new);
             }
         }
     }

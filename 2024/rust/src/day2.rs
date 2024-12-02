573a429e0bc3e8950bd45174bbbf8a62a93fe93c
Luna Borella <ulfrthewolf@gmail.com>
2024-12-02 16:32:35 +1000

    Refactoring

--- 2024/rust/src/day2.rs
+++ 2024/rust/src/day2.rs
@@ -1,29 +1,43 @@
-use itertools::Itertools;
 use crate::AOContext;
+use itertools::Itertools;
 
-fn check_distances(line: impl Iterator<Item = (i32, i32)> + Clone) -> bool {
-    let descending = line.clone()
-        .map(|w| w.1 - w.0)
+// Checks that a line is safe. Takes in a list of distances
+fn check_distances(line: impl Iterator<Item = i32> + Clone) -> bool {
+    let descending = line
+        .clone()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
         .all(|d| (-3..=-1).contains(&d));
     let ascending = line
-        .map(|w| w.1 - w.0)
+        .clone()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
         .all(|d| (1..=3).contains(&d));
     descending || ascending
 }
 
+// Creates an iterator over the list of integers that skips the ith element
+fn skip_iter(line: &[i32], i: usize) -> impl Iterator<Item = i32> + Clone + '_ {
+    line.iter()
+        .enumerate()
+        .filter(move |&(j, _)| j != i)
+        .map(|(_, x)| *x)
+}
+
 pub fn day2(input: String, ctx: &mut AOContext) {
-    let lines = input.lines()
-        .map(|l| l.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect_vec());
+    let lines = input.lines().map(|l| {
+        l.split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect_vec()
+    });
 
     let mut safe = 0;
     let mut almost_safe = 0;
 
     for line in lines {
-        if check_distances(line.iter().cloned().tuple_windows()) {
+        if check_distances(line.iter().cloned()) {
             safe += 1;
-        } else if (0..line.len()).any(|i| check_distances(line.iter().enumerate().filter(|&(j, _)| j != i).map(|(_, x)| *x).tuple_windows())) {
-            // iterators are so readable and concise trust me bro just put that above line into
-            // your code everyone will love it
+        } else if (0..line.len()).any(|i| check_distances(skip_iter(&line, i))) {
             almost_safe += 1;
         }
     }

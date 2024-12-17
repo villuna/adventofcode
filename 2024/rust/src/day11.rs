abca015fd31d6bc43076d3d741f214f3ae084214
Luna Borella <ulfrthewolf@gmail.com>
2024-12-17 14:23:55 +1000

    Day 11 optimisation

--- 2024/rust/src/day11.rs
+++ 2024/rust/src/day11.rs
@@ -1,47 +1,49 @@
 use ahash::{HashMap, HashMapExt};
 use aoc::utils::split;
 use aoc::AOContext;
-use smallvec::SmallVec;
-use smallvec::smallvec;
 
-fn next(num: u64) -> SmallVec<[u64; 2]> {
+fn next(num: u64) -> (u64, Option<u64>) {
     if num == 0 {
-        return smallvec![1];
+        return (1, None);
     }
     if let Some((a, b)) = split(num) {
-        return smallvec![a, b];
+        return (a, Some(b));
     }
-    smallvec![num * 2024]
+    (num * 2024, None)
 }
 
-fn run(stones: &mut HashMap<u64, u64>, cycles: u8) {
+fn run(stones: &mut HashMap<u64, u64>, next_stones: &mut HashMap<u64, u64>, cycles: u8) {
     for _ in 0..cycles {
-        *stones = std::mem::take(stones).into_iter()
-            .fold(HashMap::new(), |mut map, (n, c)| {
-                for m in next(n) {
-                    *map.entry(m).or_default() += c;
-                }
-                map
-            });
+        std::mem::swap(next_stones, stones);
 
+        for (n, c) in next_stones.drain() {
+            let (a, b) = next(n);
+            *stones.entry(a).or_default() += c;
+            if let Some(b) = b {
+                *stones.entry(b).or_default() += c;
+            }
+        }
+
+        next_stones.clear();
     }
 }
 
 pub fn day11(input: String, ctx: &mut AOContext) {
-    let mut ints: HashMap<u64, u64> = input.split_whitespace()
+    let mut stones = input.split_whitespace()
         .map(|x| x.parse::<u64>().unwrap())
         .fold(HashMap::new(), |mut map, n| {
             *map.entry(n).or_default() += 1;
             map
         });
 
+    let mut next_stones = HashMap::new();
+
     ctx.parsing_done();
 
-    run(&mut ints, 25);
-    ctx.submit_part1(ints.values().sum::<u64>());
+    run(&mut stones, &mut next_stones, 25);
+    ctx.submit_part1(stones.values().sum::<u64>());
 
 
-    run(&mut ints, 50);
-    ctx.submit_part2(ints.values().sum::<u64>());
+    run(&mut stones, &mut next_stones, 50);
+    ctx.submit_part2(stones.values().sum::<u64>());
 }
-
